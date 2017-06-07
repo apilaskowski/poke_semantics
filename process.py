4bfaa224d5f5f81f166be1aeeef4d024c6c894ff
Artur Laskowski <artur.laskowski@student.put.poznan.pl>
2017-06-07 21:48:53 +0200

    ADD: Initial commit.

---
+++ process.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import requests
+import pickle
+import tqdm
+import numpy as np
+
+chart_file = open("types_chart.txt", "r")
+
+types = [line.strip() for line in open("types.txt", "r")]
+
+chart = np.loadtxt("types_chart.txt")
+
+types_strengths = {}
+for i, type_defend in enumerate(types):
+    for j, type_attack in enumerate(types):
+        types_strengths[(type_defend, type_attack)] = chart[i, j]
+
+
