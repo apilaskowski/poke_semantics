49d33f8a8679e0c9581eafa72c2af6429b38a351
Artur Laskowski <artur.pi.laskowski@gmail.com>
2017-06-08 02:59:37 +0200

    The end.

--- graphtest.py
+++ graphtest.py
@@ -4,8 +4,11 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from random import sample, random, choice
+from process import get_graph
+import numpy as np
 
-g = nx.Graph()
+
+g = nx.DiGraph()
 min_size = 0.01
 max_size = 0.3
 
@@ -17,18 +20,45 @@ def scale_func(x):
 
 edge_color = "magenta"
 edge_width = 0.5
+best_n = 15
 nn = 151
-ne = int(nn ** 2 * 0.02)
+# ne = int(nn ** 2 * 0.02)
 imgs = []
+
+
 for node_i in range(nn):
     g.add_node(node_i)
     imgs.append(mpimg.imread('sprites/{}.png'.format(node_i + 1)))
-for edge_i in range(ne):
-    x, y = sample(range(nn), 2)
-    if (x, y) not in g.edges():
-        g.add_edge(x, y, color=choice(["pink","magenta"]), weight=choice([2, 4]) / 8)
+# for edge_i in range(ne):
+#     x, y = sample(range(nn), 2)
+#     if (x, y) not in g.edges():
+#         g.add_edge(x, y, color=choice(["pink","magenta"]), weight=choice([2, 4]) / 8)
 
-pos = nx.random_layout(g)
+perfect_graph = get_graph()
+
+im_sizes = np.zeros(nn)
+for (p1,p2), w in get_graph().items():
+    i0 = int(p1.id[1:])-1
+    i1 = int(p2.id[1:])-1
+    if w ==2:
+        color = "pink"
+    else:
+        color = "magenta"
+    im_sizes[i0]+=w
+
+    g.add_edge(i0,i1, color=color, weight=w/8)
+
+pos = nx.fruchterman_reingold_layout(g)
+
+
+im_sizes -= im_sizes.min()
+im_sizes /= im_sizes.max()
+
+if best_n is not None:
+    worst_i = np.argsort(im_sizes)[::-1][best_n:]
+    for i in worst_i:
+        g.remove_node(i)
+
 
 edges = g.edges()
 edge_colors = [g[u][v]['color'] for u, v in edges]
@@ -47,7 +77,7 @@ for node in g.nodes():
     x, y = pos[node]
     xx, yy = trans((x, y))  # figure coordinates
     xa, ya = trans2((xx, yy))  # axes coordinates
-    w = random()
+    w = im_sizes[node]
     imsize = scale_func(w)
     a = plt.axes([xa - imsize / 2.0, ya - imsize / 2.0, imsize, imsize])
     a.imshow(imgs[node])
--- process.py
+++ process.py
@@ -1,60 +1,62 @@
 #!/usr/bin/env python3
 
-import requests
-import pickle
-import tqdm
-import numpy as np
-from collections import namedtuple
-from collections import Counter
+def get_graph():
+    import requests
+    import pickle
+    import tqdm
+    import numpy as np
+    from collections import namedtuple
+    from collections import Counter
 
-chart_file = open("types_chart.txt", "r")
+    chart_file = open("types_chart.txt", "r")
 
-types = [line.strip() for line in open("types.txt", "r")]
+    types = [line.strip() for line in open("types.txt", "r")]
 
-chart = np.loadtxt("types_chart.txt")
+    chart = np.loadtxt("types_chart.txt")
 
-types_strengths = {}
-for i, type_attack in enumerate(types):
-    for j, type_defend in enumerate(types):
-        types_strengths[(type_attack, type_defend)] = chart[i, j]
+    types_strengths = {}
+    for i, type_attack in enumerate(types):
+        for j, type_defend in enumerate(types):
+            types_strengths[(type_attack, type_defend)] = chart[i, j]
 
 
-pokemons_file = [line.strip() for line in open("pokemon.csv", "r")]
+    pokemons_file = [line.strip() for line in open("pokemon.csv", "r")]
 
-Pokemon = namedtuple('Pokemon', ['id', 'name', 'type'])
+    Pokemon = namedtuple('Pokemon', ['id', 'name', 'type'])
 
-pokemons = []
+    pokemons = []
 
-cnt = Counter()
+    cnt = Counter()
 
-for pokemon_line in pokemons_file:
-    tab = pokemon_line.split(';')
-    if tab[1].startswith("#"):
-        if not tab[2] in cnt:
-            cnt[tab[2]] += 1
-            pokemons.append(Pokemon(tab[1], tab[2], tuple(tab[4:])))
-    elif tab[0].startswith("#"):
-        if not tab[2] in cnt:
-            cnt[tab[2]] += 1
-            pokemons.append(Pokemon(tab[0], tab[1], tuple(tab[3:])))
+    for pokemon_line in pokemons_file:
+        tab = pokemon_line.split(';')
+        if tab[1].startswith("#"):
+            if not tab[2] in cnt:
+                cnt[tab[2]] += 1
+                pokemons.append(Pokemon(tab[1], tab[2], tuple(tab[4:])))
+        elif tab[0].startswith("#"):
+            if not tab[2] in cnt:
+                cnt[tab[2]] += 1
+                pokemons.append(Pokemon(tab[0], tab[1], tuple(tab[3:])))
+    print(len(pokemons))
 
-print(len(pokemons))
+    graph = {}
 
-graph = {}
+    for attacker in pokemons:
+        for defender in pokemons:
+            ratio = 1.0
+            for a_ptype in attacker[2]:
+                for d_ptype in defender[2]:
+                    ratio *= types_strengths[a_ptype, d_ptype]
+            graph[(attacker, defender)] = ratio
 
-for attacker in pokemons:
-    for defender in pokemons:
-        ratio = 1.0
-        for a_ptype in attacker[2]:
-            for d_ptype in defender[2]:
-                ratio *= types_strengths[a_ptype, d_ptype]
-        graph[(attacker, defender)] = ratio
+    perfect_graph = {}
 
-perfect_graph = {}
+    good_edge = 0
+    for attacker in pokemons:
+        for defender in pokemons:
+            if graph[attacker, defender] > 1:
+                good_edge += 1
+                perfect_graph[attacker, defender] = graph[attacker, defender]
 
-good_edge = 0
-for attacker in pokemons:
-    for defender in pokemons:
-        if graph[attacker, defender] > 1:
-            good_edge += 1
-            perfect_graph[attacker, defender] = graph[attacker, defender]
+    return perfect_graph
